e00ad9a8d1ac53c51ad70d831d4911feb36de89b
Bearded Avenger <email@nickhaskins.com>
2014-11-17 09:54:30 -0600

    let .org handle updating cmb2
    this is now just for custom fields used in cmb2

--- cgc-meta.php
+++ cgc-meta.php
@@ -3,21 +3,10 @@
 /**
 *
 *
-*	Plugin Name: CGC Meta
+*	Plugin Name: CGC Meta - Custom Fields
+*	Description:
 */
 
-if( !class_exists( 'CMB2' ) ) {
-	require_once( 'init.php' );
-	require_once('custom-fields/select2/cmb-field-select2.php');
-}
-
-/* Custom Field Types */
-
-/**
- * Gets a number of posts and displays them as options
- * @param  array $query_args Optional. Overrides defaults.
- * @return array             An array of options that matches the CMB options array
- */
 function cgc_get_posts_for_cmb( $query_args ) {
 
     $args = wp_parse_args( $query_args, array(
